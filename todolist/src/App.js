4c0702a9f1518f5a78a92f95f3805e02b2f3ced6
kwakmj <mj.kwak@sqisoft.com>
2021-07-28 10:46:52 +0900

    To do List Component 추가

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+/* eslint-disable */
+import React from 'react';
 import './App.css';
+import { createGlobalStyle } from 'styled-components';
+import TodoTemplate from './components/TodoTemplate';
+import TodoHead from './components/TodoHead';
+import TodoList from './components/TodoList';
+import TodoCreate from './components/TodoCreate';
+
+// 특정 컴포넌트를 만들어서 스타일링 하는 것이 아니라 글로벌 스타일 추가하고 싶은 경우 createGlobalStyle 사용
+const BodyStyled = createGlobalStyle`
+  body {
+    background: #e9ecef;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+      <BodyStyled />
+      <TodoTemplate>
+        <TodoHead />
+        <TodoList />
+        <TodoCreate />
+      </TodoTemplate>
+    </React.Fragment>
+  )
 }
-
 export default App;
---
+++ todolist/src/components/TodoCreate.js
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import styled, { css } from 'styled-components';
+import { MdAdd } from 'react-icons/md';
+
+const CircleButtonStyled = styled.button`
+    background: #38d9a9;
+    &:hover {
+        background: #63e6be;
+    }
+    &:active {
+        background: #20c997;
+    }
+    z-index: 5;
+    cursor: pointer;
+    width: 80px;
+    height: 80px;
+    display: block;
+    align-items: center;
+    justify-content: center;
+    font-size: 60px;
+    position: absolute;
+    left: 50%;
+    bottom: 0px;
+    transform: translate(-50%, 50%);
+    color: white;
+    border-radius: 50%;
+    border: none;
+    outline: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: 0.125s all ease-in;
+    ${props =>
+        props.open &&
+        css`
+            background: #ff6b6b;
+            &:hover {
+                background: #ff8787;
+            }
+            &:active {
+                background: #fa5252;
+            }
+            transform: translate(-50%, 50%) rotate(45deg);
+        `}
+`;
+
+const InsertFormPositionerStyled = styled.div`
+    width: 100%;
+    bottom: 0;
+    left: 0;
+    position: absolute;
+`;
+
+const InsertFormStyled = styled.form`
+    background: #f8f9fa;
+    padding: 32px 32px 72px 32px;
+    border-bottom-left-radius: 16px;
+    border-bottom-right-radius: 16px;
+    border-top: 1px solid #e9ecef;
+`;
+
+const InputStyled = styled.input`
+    padding: 12px;
+    border-radius: 4px;
+    border: 1px solid #dee2e6;
+    width: 100%;
+    outline: none;
+    font-size: 18px;
+    box-sizing: border-box;
+`;
+
+export default function TodoCreate() {
+    const [open, setOpen] = useState(false);
+    const onToggle = () => setOpen(!open);
+
+    return (
+        <React.Fragment>
+            {open && (
+                <InsertFormPositionerStyled>
+                    <InsertFormStyled>
+                        <InputStyled autoFocus placeholder='할 일을 입력 후, Enter를 누르세요.' />
+                    </InsertFormStyled>
+                </InsertFormPositionerStyled>
+            )}
+            <CircleButtonStyled onClick={onToggle} open={open}>
+                <MdAdd />
+            </CircleButtonStyled>
+        </React.Fragment>
+    );
+}
